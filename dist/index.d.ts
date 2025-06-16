7409e514f9a97acc3357fcc9811712bff6ecd2b8
nilcamprubi <nilcamprubivilademunt@gmail.com>
2025-06-16 10:00:24 +0200

    Built

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 export { default as BubbleMenu } from './BubbleMenu';
 export type { BubbleMenuStyleProps } from './BubbleMenu';
 export type { BubbleProps } from './BubbleWrapper';
+export type { BubbleWrapperProps } from './BubbleWrapper';
+export type { BubbleStyleProps } from './BubbleWrapper';
